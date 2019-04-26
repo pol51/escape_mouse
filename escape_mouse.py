12c487b694aea0255ee57fc1f8779b4630fbb61c
Paul Labedan <plabedan@gmail.com>
2019-04-26 22:49:45 +0200

    cat follows mouse

--- escape_mouse.py
+++ escape_mouse.py
@@ -1,12 +1,21 @@
+from enum import IntFlag, IntEnum
 import pygame
+from pygame.math import Vector2
 
-CASE_SIZE = 64
+CASE_SIZE = 96
 
-WALL = 0
-PATH = 1
-MOUSE = 2
-CAT = 4
-GOAL = 8
+class Item(IntFlag):
+  WALL = 0
+  PATH = 1
+  MOUSE = 2
+  CAT = 4
+  GOAL = 8
+  
+class Move:
+  LEFT = Vector2(-1, 0)
+  RIGHT = Vector2(1, 0)
+  UP = Vector2(0, -1)
+  DOWN = Vector2(0, 1)
 
 world = [
   [0,0,0,0,0,0,0,0,0,0],
@@ -23,48 +32,53 @@ world = [
 
 
 class Persona:
-    def __init__(self, img, x=0, y=0):
+    def __init__(self, img, pos=Vector2(0, 0)):
         sprite = pygame.image.load(img)
         self.sprite = pygame.transform.scale(sprite, (CASE_SIZE, CASE_SIZE))
-        self.x = x
-        self.y = y
+        self.pos = pos
+        self.follower = None
+        self.moves = []
         
+    def follow(self, other, path_to_other):
+        self.moves = path_to_other
+        other.follower = self
+
     def draw(self, screen):
-        screen.blit(self.sprite, (self.x * CASE_SIZE, self.y * CASE_SIZE))
+        screen.blit(self.sprite, (self.pos.x * CASE_SIZE, self.pos.y * CASE_SIZE))
         
-    def move_up(self):
-        if world[self.y-1][self.x] & PATH:
-            self.y -= 1
-
-    def move_down(self):
-        if world[self.y+1][self.x] & PATH:
-            self.y += 1
-
-    def move_left(self):
-        if world[self.y][self.x-1] & PATH:
-            self.x -= 1
-
-    def move_right(self):
-        if world[self.y][self.x+1] & PATH:
-            self.x += 1
-
-
+    def move(self, move):
+        pos = self.pos + move
+        if world[int(pos.y)][int(pos.x)] & Item.PATH:
+            self.pos = pos
+            if self.follower:
+                self.follower.moves.append(move)
+        if self.follower:
+            self.follower.move_next()
+            
+    def move_next(self):
+        if len(self.moves):
+          move, *self.moves = self.moves
+          self.move(move)
+          
+            
 def get_init_world_position(flag):
   for i in range(len(world)):
-      for j in range(len(world[0])):
+      for j in range(len(world[i])):
           if world[i][j] & flag:
-              return (j, i)
+              return Vector2(j, i)
         
 def main():
     pygame.init()
 
     pygame.display.set_caption('Escape Mouse!')
-    screen = pygame.display.set_mode((CASE_SIZE*len(world[0]), CASE_SIZE*len(world)))
+    screen_size = ((CASE_SIZE*len(world[0]), CASE_SIZE*len(world)))
+    screen = pygame.display.set_mode(screen_size)
     
     personas = list()
-    mouse = Persona('data/mouse_128px.png', *get_init_world_position(MOUSE))
-    cat = Persona('data/cat_128px.png', *get_init_world_position(CAT))
-    goal_pos = get_init_world_position(GOAL)
+    mouse = Persona('data/mouse_128px.png', get_init_world_position(Item.MOUSE))
+    cat = Persona('data/cat_128px.png', get_init_world_position(Item.CAT))
+    cat.follow(mouse, [Move.DOWN, Move.DOWN, Move.DOWN])
+    goal_pos = get_init_world_position(Item.GOAL)
     personas.append(mouse)
     personas.append(cat)
     
@@ -75,24 +89,34 @@ def main():
                 running = False
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
-                    mouse.move_left()
+                    mouse.move(Move.LEFT)
                 if event.key == pygame.K_RIGHT:
-                    mouse.move_right()
+                    mouse.move(Move.RIGHT)
                 if event.key == pygame.K_UP:
-                    mouse.move_up()
+                    mouse.move(Move.UP)
                 if event.key == pygame.K_DOWN:
-                    mouse.move_down()
+                    mouse.move(Move.DOWN)
         screen.fill((0,0,0))
         for i in range(len(world)):
-            for j in range(len(world[0])):
+            for j in range(len(world[i])):
                 pygame.draw.rect(screen,
-                    world[i][j] & PATH and (255,255,255) or (127,127,127),
+                    world[i][j] & Item.PATH and (255,255,255) or (127,127,127),
                     (j*CASE_SIZE, i*CASE_SIZE, CASE_SIZE, CASE_SIZE), 0)
         pygame.draw.rect(screen,
                     (255,127,0),
-                    (goal_pos[0]*CASE_SIZE, goal_pos[1]*CASE_SIZE, CASE_SIZE, CASE_SIZE), 0)
+                    (goal_pos.x*CASE_SIZE, goal_pos.y*CASE_SIZE, CASE_SIZE, CASE_SIZE), 0)
         for p in personas:
             p.draw(screen)
+        if cat.pos == mouse.pos:
+            from PIL import Image, ImageFilter
+            pil_blured = Image.frombytes("RGBA", screen_size, screen.get_view().raw).filter(ImageFilter.GaussianBlur(radius=6))
+            screen.blit(pygame.image.fromstring(pil_blured.tobytes("raw", 'RGBA'), screen_size, 'RGBA'), (0, 0))
+            pygame.display.flip()
+            while running:
+              for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+            
         pygame.display.flip()
 
 
